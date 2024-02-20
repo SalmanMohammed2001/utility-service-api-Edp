5b007d925b1ee2a557bd3bc2b0928696ff53a799
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-02-20 13:06:37 +0530

    mongodb config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import * as process from "process";
 
+import bodyParser from "body-parser";
+import  mongoose from "mongoose";
+import express from 'express'
+import cors from 'cors'
+
 const dotenv=require('dotenv')
 dotenv.config({ path: 'src/.env' });
 
-console.log(process.env.SERVER_PORT)
+const app=express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+const  port=process.env.SERVER_PORT
+
+
+try {
+    mongoose.connect("mongodb://127.0.0.1:27017/utility_db").then(()=>{
+        app.listen(port,()=>{
+            console.log(`serve running ${port}`)
+        })
+    })
+
+}catch (err){
+    console.log(err)
+}
